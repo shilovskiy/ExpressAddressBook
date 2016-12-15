3bd20284002e8153058f7010d9eafa8beaa1968c
shilovskiy <shilovskiy@gmail.com>
2016-12-15 23:15:32 +0300

    add new features

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{'message':err.message,'error':err});
 });
 
 module.exports = app;
--- routes/edituserdata.js
+++ routes/edituserdata.js
@@ -15,25 +15,45 @@ router.get('/add', function(req, res, next) {
 
 router.get('/save', function(req, res, next) {
     //let id = require('mongodb').ObjectID(req.params.id);
-    let Name = req.query.Name;
-    let LName = req.query.LName;
-    let Email = req.query.Email;
-    let Phone = req.query.Phone;
-    let Skype = req.query.Skype;
+    let Cntct ={};
+    let OtherContacts= {};
+    if (req.query.Name!=undefined){
+        Cntct.Name = req.query.Name
+    }
+    if (req.query.LName!=undefined){
+        Cntct.LName = req.query.LName
+    }
+    if (req.query.Phone!=undefined){
+        Cntct.Phone = req.query.Phone
+    }
+    if (req.query.Email!=undefined){
+        OtherContacts.Email = req.query.Email
+    }
+    if (req.query.Skype!=undefined){
+        OtherContacts.Skype = req.query.Skype
+    }
+    Cntct.OtherContacts=OtherContacts;
+   //Cntct.push(OtherContacts);
+
+
+
     var collection = db.get().collection('Contacts');
 
    // let newContact ={'Name':Name,'LName':LName,'Phone':Phone,'OtherContacts.Email':Email,'OtherContacts.Skype':Skype};
-    collection.insertOne({'Name':Name,'LName':LName,'Phone':Phone}
+    collection.insertOne(Cntct//{'Name':Name,'LName':LName,'Phone':Phone}
 
-    ,(err,res)=> {
+    ,(err,reults)=> {
             if (err) {
                 console.log(err);
+                var err = new Error(err.message);
+                next(err);
             } else {
-                console.log(`Inserted ${res.length} devices with "_id" are: ${JSON.stringify(res)}`);
+                console.log(`Inserted ${reults.length} contact with "_id" are: ${JSON.stringify(reults)}`);
+                res.redirect(301,"/");
             }
         });
 
-    res.redirect(301,"/");
+
 });
 
 router.get('/:id', function(req, res, next) {
